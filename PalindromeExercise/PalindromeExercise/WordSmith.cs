9785e21d4d97a3194bf06f2aa279f1aa7aaab288
Anthony Rodriguez <anthonyr38@proton.me>
2023-06-05 20:31:44 -0400

    TDD Palindrome Exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word) 
+        {
+            //throw new NotImplementedException();
+            var revWord = "";
+            for(int revd = word.Length-1; revd >= 0; revd--)
+            {
+                revWord += word[revd];
+            }
+           
+            if (word.ToLower() == revWord.ToLower()) 
+            {
+                return true;
+            }
+            else 
+            {
+                return false; 
+            }
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
+using PalindromeExercise;
 using System;
+using System.Net.WebSockets;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("civic", true)]
+        [InlineData("crown", false)]
+        [InlineData("tacocat", true)]
+        [InlineData("racecar", true)]
+        [InlineData("random", false)]
 
+        public void PalindromeTest(string pal1, bool expected)
+        {
+            //Arange
+            var wordSmith = new WordSmith();
+            //Act
+            var actual = wordSmith.IsAPalindrome(pal1);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
